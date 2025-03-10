33d286639a43a8a205569bdc698d52e8b78aa465
Drew Maughan <drew.maughan@toptal.com>
2025-03-10 18:22:15 +0000

    Setting up a basic page layout using Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="p-3">
+        <div class="container">
+          <h1>Colour Contrast Table Thing</h1>
+        </div>
       </header>
+
+      <main class="p-3">
+        <div class="container">
+          <p>The content goes here.</p>
+        </div>
+      </main>
+
+      <footer>
+        <div class="container fs-6 text-center p-2">
+          Copyright &copy; Drew Maughan, all rights reserved.
+        </div>
+      </footer>
     </div>
   );
 }
