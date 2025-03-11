df41437b09fe1ad30ae4d01b83b8be6d5025b58a
Drew Maughan <drew.maughan@toptal.com>
2025-03-11 10:01:43 +0000

    Generating a table based on provided values.
    Being reminded of how to use a loop inside a template!

--- src/App.js
+++ src/App.js
@@ -2,22 +2,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  let colours = ["#FF0000", "#FFFF00", "#C022F0", "#0000FF"];
+
   return (
     <div className="App">
-      <header class="p-3">
-        <div class="container">
+      <header className="p-3">
+        <div className="container">
           <h1>Colour Contrast Table Thing</h1>
         </div>
       </header>
 
-      <main class="p-3">
-        <div class="container">
+      <main className="p-3">
+        <div className="container">
           <p>The content goes here.</p>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>&nbsp;</th>
+                {colours.map((hex, index) => (
+                  <th key={'col-' + index}>{hex}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+            {colours.map((rowHex, rowIndex) => (
+              <tr>
+                <th scope="row" key={'row-' + rowIndex}>{rowHex}</th>
+                {colours.map((columnHex, columnIndex) => (
+                  <td key={`cell-${rowIndex}-${columnIndex}`}>{columnHex}</td>
+                ))}
+              </tr>
+            ))}
+            </tbody>
+          </table>
         </div>
       </main>
 
       <footer>
-        <div class="container fs-6 text-center p-2">
+        <div className="container fs-6 text-center p-2">
           Copyright &copy; Drew Maughan, all rights reserved.
         </div>
       </footer>
